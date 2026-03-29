62b00f496af91b6259cdacb9f6114911f33c5df6
SLIM-TECH <marlensardarbekov68@gmail.com>
2026-03-29 12:55:15 +0600

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,34 @@
+﻿from .shikimori import (
+    search_anime,
+    search_manga,
+    get_top_anime,
+    get_anime_by_genre,
+    get_anime_by_season,
+    get_anime_detail,
+    get_manga_detail,
+    get_anime_videos,
+    get_genres,
+    get_image_url,
+    get_shikimori_url,
+    get_shikimori_manga_url,
+    score_to_stars,
+)
+from .kodik import get_episode_sources, get_best_episode_media_url
+
+__all__ = [
+    "search_anime",
+    "search_manga",
+    "get_top_anime",
+    "get_anime_by_genre",
+    "get_anime_by_season",
+    "get_anime_detail",
+    "get_manga_detail",
+    "get_anime_videos",
+    "get_genres",
+    "get_image_url",
+    "get_shikimori_url",
+    "get_shikimori_manga_url",
+    "score_to_stars",
+    "get_episode_sources",
+    "get_best_episode_media_url",
+]
